e9e7d258ca2770d2058f9d3df6216f3ddcfe9fd4
oscar1delaguila <71659831+oscar1delaguila@users.noreply.github.com>
2020-09-21 18:45:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+
+
+console.log('Bienvenido a JS para programadores');
---
+++ sum.spec.js
@@ -0,0 +1,22 @@
+
+import sum from './sum';
+
+test('adds 1 + 2 to equal 3', () => {
+   expect(sum(1, 2)).toBe(3);
+});
+
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 4)).toBe(5);
+ });
+
+ 
+ test('adds 1 + 2 to equal 3', () => {
+    expect(sum(4, 2)).toBe(6);
+ });
+
+ 
+ test('adds 1 + 2 to equal 3', () => {
+    expect(sum(5, 2)).toBe(7);
+ });
+ 
